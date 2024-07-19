a528408d7f0a047c69a24656dcbf71392ebb12ea
Derkin <coverforhobbyit@gmail.com>
2024-07-19 20:12:56 +0200

    added Details

---
+++ JanusApp/JanusApp/DetailRowView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailRowView.swift
+//  JanusApp
+//
+//  Created by Raffaele Siciliano on 19/07/24.
+//
+
+import SwiftUI
+
+struct DetailRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailRowView()
+}
---
+++ JanusApp/JanusApp/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  JanusApp
+//
+//  Created by Raffaele Siciliano on 19/07/24.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
